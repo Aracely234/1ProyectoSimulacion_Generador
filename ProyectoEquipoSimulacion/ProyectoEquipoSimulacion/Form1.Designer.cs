d457ae7395bce267a95861184a4882ba01889595
Aracely234 <84871755+Aracely234@users.noreply.github.com>
2021-06-04 23:15:54 -0700

    Primer Avance

---
+++ ProyectoEquipoSimulacion/ProyectoEquipoSimulacion/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿
+namespace ProyectoEquipoSimulacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbA = new System.Windows.Forms.TextBox();
+            this.tbC = new System.Windows.Forms.TextBox();
+            this.tbM = new System.Windows.Forms.TextBox();
+            this.tbXo = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbn = new System.Windows.Forms.TextBox();
+            this.ColumnaNumero = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumaNumeroGenerado = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblpromedio = new System.Windows.Forms.Label();
+            this.lbldisnor = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "a:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "c:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Xo:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(45, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "M:";
+            // 
+            // tbA
+            // 
+            this.tbA.Location = new System.Drawing.Point(88, 57);
+            this.tbA.Name = "tbA";
+            this.tbA.Size = new System.Drawing.Size(108, 20);
+            this.tbA.TabIndex = 4;
+            // 
+            // tbC
+            // 
+            this.tbC.Location = new System.Drawing.Point(88, 83);
+            this.tbC.Name = "tbC";
+            this.tbC.Size = new System.Drawing.Size(108, 20);
+            this.tbC.TabIndex = 5;
+            // 
+            // tbM
+            // 
+            this.tbM.Location = new System.Drawing.Point(88, 135);
+            this.tbM.Name = "tbM";
+            this.tbM.Size = new System.Drawing.Size(108, 20);
+            this.tbM.TabIndex = 7;
+            // 
+            // tbXo
+            // 
+            this.tbXo.Location = new System.Drawing.Point(88, 109);
+            this.tbXo.Name = "tbXo";
+            this.tbXo.Size = new System.Drawing.Size(108, 20);
+            this.tbXo.TabIndex = 6;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnaNumero,
+            this.ColumaNumeroGenerado});
+            this.dataGridView1.Location = new System.Drawing.Point(38, 170);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(244, 268);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(242, 128);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Generar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Sitka Banner", 12F);
+            this.label5.Location = new System.Drawing.Point(238, 60);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(133, 23);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Numeros a generar:";
+            // 
+            // tbn
+            // 
+            this.tbn.Location = new System.Drawing.Point(377, 64);
+            this.tbn.Name = "tbn";
+            this.tbn.Size = new System.Drawing.Size(108, 20);
+            this.tbn.TabIndex = 11;
+            // 
+            // ColumnaNumero
+            // 
+            this.ColumnaNumero.HeaderText = "Numero";
+            this.ColumnaNumero.Name = "ColumnaNumero";
+            // 
+            // ColumaNumeroGenerado
+            // 
+            this.ColumaNumeroGenerado.HeaderText = "Numero Pseudoalatorio";
+            this.ColumaNumeroGenerado.Name = "ColumaNumeroGenerado";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Sitka Banner", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(299, 381);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(278, 23);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Promedio de los numeros pseodoaleatorios:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Sitka Banner", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(299, 408);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(138, 23);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Distribución normal:";
+            // 
+            // lblpromedio
+            // 
+            this.lblpromedio.AutoSize = true;
+            this.lblpromedio.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblpromedio.Location = new System.Drawing.Point(583, 384);
+            this.lblpromedio.Name = "lblpromedio";
+            this.lblpromedio.Size = new System.Drawing.Size(0, 16);
+            this.lblpromedio.TabIndex = 14;
+            // 
+            // lbldisnor
+            // 
+            this.lbldisnor.AutoSize = true;
+            this.lbldisnor.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbldisnor.Location = new System.Drawing.Point(443, 411);
+            this.lbldisnor.Name = "lbldisnor";
+            this.lbldisnor.Size = new System.Drawing.Size(0, 16);
+            this.lbldisnor.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Sitka Banner", 12F);
+            this.label8.Location = new System.Drawing.Point(278, 18);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(256, 23);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Generador de Numeros Pseodoalarorios";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lbldisnor);
+            this.Controls.Add(this.lblpromedio);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tbn);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.tbM);
+            this.Controls.Add(this.tbXo);
+            this.Controls.Add(this.tbC);
+            this.Controls.Add(this.tbA);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbA;
+        private System.Windows.Forms.TextBox tbC;
+        private System.Windows.Forms.TextBox tbM;
+        private System.Windows.Forms.TextBox tbXo;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnaNumero;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumaNumeroGenerado;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblpromedio;
+        private System.Windows.Forms.Label lbldisnor;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ ProyectoEquipoSimulacion/ProyectoEquipoSimulacion/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoEquipoSimulacion
+{
+    public partial class Form1 : Form
+    {
+        //Declaracion de variables
+        public static double a;
+        public static double c ;
+        public static double Xo;
+        public static double M ;
+        double n, modulo, m = 0, acumulador = 0, promedio, Zo;
+        double[] random = new double[100000];
+        int ad;
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            double.TryParse(tbA.Text, out a);
+            double.TryParse(tbC.Text, out c);
+            double.TryParse(tbXo.Text, out Xo);
+            double.TryParse(tbM.Text, out M);
+            double.TryParse(tbn.Text, out n);
+            //generar numeros 
+            for (int i = 0; i < n; i++)
+            {
+                modulo = (a * Xo + c) % M;
+                random[i] = modulo / M;
+                double redoneado = (Math.Round(random[i], 5));
+                ad = dataGridView1.Rows.Add();
+                dataGridView1.Rows[ad].Cells[0].Value = (i+1).ToString();
+                dataGridView1.Rows[ad].Cells[1].Value = redoneado.ToString();
+               
+                acumulador += random[i];
+
+                m = modulo;
+                Xo = m;
+                
+                
+            }
+            
+            //obtenemos el promedio de los números pseodoaleatorios
+            promedio = acumulador / n;
+            lblpromedio.Text = (Math.Round(promedio,5)).ToString();
+
+            //obtenemos la distribución normal
+            Zo = ((Math.Sqrt(n)) * (promedio - 0.5)) / (Math.Sqrt(0.083333333));
+            lbldisnor.Text = (Math.Round(Zo, 5)).ToString();
+
+
+
+        }
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+    }
+}
